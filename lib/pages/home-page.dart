987b5ed79131f5187791907a20170861e7e76c1a
ijovanovski <ijovanovski@gmail.com>
2020-10-12 21:32:35 +0200

    completed

--- lib/pages/home-page.dart
+++ lib/pages/home-page.dart
@@ -1,13 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 import '../drawer.dart';
-import '../name_card_widget.dart';
-
-
-
-
-
-
 
 class HomePage extends StatefulWidget {
   @override
@@ -15,24 +10,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var myText = "change my name try";
-  TextEditingController _nameCotroller = TextEditingController();
+  // var myText = "change my name try";
+  // TextEditingController _nameCotroller = TextEditingController();
+  var url = 'http://jsonplaceholder.typicode.com/photos';
+  var data;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    var res = await http.get(url);
+    data = jsonDecode(res.body);
+    setState(() {});
+  }
+  //   @override
+  // void dispose() {
+  //   super.disposed();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(title: Text("Awesome App")),
-      body: Center(
-          child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: NameCardWidget(myText: myText, nameCotroller: _nameCotroller),
-      )
-      ),
+      body: data != null
+          ? ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                    title: Text(data[index]["title"]),
+                    subtitle: Text("${data[index]["id"]})"),
+                    leading: Image.network(data[index]["url"]));
+              },
+              itemCount: data.length)
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
       drawer: MyDrawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          myText = _nameCotroller.text;
+          // myText = _nameCotroller.text;
           setState(() {});
         },
         child: Icon(Icons.edit),
